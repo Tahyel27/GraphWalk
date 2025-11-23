c93536ff22389d4d30572f57de73ef87e35adf7d
David Sponer <sponderdavid@gmail.com>
2025-11-23 13:14:37 +0100

    Implemented run

--- include/MCSimulation.cpp
+++ include/MCSimulation.cpp
@@ -8,6 +8,19 @@ MCSimulation::MCSimulation(const Graph &graph_, int runs, long long steps)
     totalRuns = runs;
 }
 
+void MCSimulation::run()
+{
+    //iterate over multiple runs
+    for (size_t i = 0; i < totalRuns; i++)
+    {
+        //perform a run
+        for (size_t i = 0; i < totalSteps; i++)
+        {
+            step(i);
+        }
+    }    
+}
+
 MCSimulation::data_t MCSimulation::getData()
 {
     return data.i;

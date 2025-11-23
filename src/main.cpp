aef430317abd3cf62822772710d32a08fb04af73
David Sponer <sponderdavid@gmail.com>
2025-11-23 13:31:52 +0100

    added print nodes and a small test

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
+#include <format>
 
 #include "Graph.hpp"
 #include "MCSimulation.hpp"
+
+void printNodes(const MCSimulation::Nodes &data)
+{
+    for (size_t i = 0; i < data.i.size(); i++)
+    {
+        std::cout << std::format("{:5} {:5}",data.x[i],data.y[i]) << std::endl;
+    }
+}
 
 int main()
 {
     Graph graph;
     MCSimulation sim(graph, 10, 100);
-    std::cout << "Hello world" << std::endl;
+
+    sim.run();
+
+    auto res = sim.getData();
+
+    printNodes(res);
+
     return 0;
 }

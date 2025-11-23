7efd91b8180acb4a2bf81d4b312d4db12e5a6da3
David Sponer <sponderdavid@gmail.com>
2025-11-23 13:18:25 +0100

    added MCSimulation to cmake project

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 #include "Graph.hpp"
+#include "MCSimulation.hpp"
 
 int main()
 {
     Graph graph;
+    MCSimulation sim(graph, 10, 100);
     std::cout << "Hello world" << std::endl;
     return 0;
 }

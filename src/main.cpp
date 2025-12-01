b34ab6efbcc21c7547af27aef5923bab1101b7d1
David Sponer <sponderdavid@gmail.com>
2025-12-01 16:37:45 +0100

    added some sensible defaults

--- src/main.cpp
+++ src/main.cpp
@@ -77,6 +77,26 @@ std::tuple<Graph, GraphCoordinates> loadFromFile(const std::string &fname)
     
 }
 
+std::tuple<Graph, GraphCoordinates> getDefaultSquareGrid()
+{
+    Graph graph;
+    graph.addNode();
+    graph.addBidirectionalEdge(0, 0, 1, 0);
+    graph.addBidirectionalEdge(0, 0, 0, 1);
+    // graph.addBidirectionalEdge(0,0,0,10);
+
+    GraphCoordinates coords;
+    coords.N = 1;
+    coords.scaleX = 1;
+    coords.scaleY = 1;
+    coords.skewX = 0;
+    coords.skewY = 0;
+    coords.X = {0};
+    coords.Y = {0};
+
+    return std::tuple<Graph, GraphCoordinates>(graph, coords);
+}
+
 void printNodes(const MCSimulation::Nodes &data)
 {
     for (size_t i = 0; i < data.i.size(); i++)
@@ -118,7 +138,8 @@ void printReturnTime(const PostProcessor &proc)
 
 int main(int argc, char *argv[])
 {
-    auto [a, b] = loadFromFile("testgrph.txt");
+    Graph graph;
+    GraphCoordinates coords;
     
     int numRuns = 1000;
     int numSteps = 1000;
@@ -127,22 +148,16 @@ int main(int argc, char *argv[])
         numSteps = atoi(argv[0]);
         numRuns = atoi(argv[1]);
     }
+    if (argc == 3)
+    {
+        std::string fname = argv[2];
+        std::tie(graph, coords) = loadFromFile(fname);
+    }
+    else
+    {
+        std::tie(graph, coords) = getDefaultSquareGrid();
+    }
     
-
-    Graph graph;
-    graph.addNode();
-    graph.addBidirectionalEdge(0,0,1,0);
-    graph.addBidirectionalEdge(0,0,0,1);
-    //graph.addBidirectionalEdge(0,0,0,10);
-
-    GraphCoordinates coords;
-    coords.N = 1;
-    coords.scaleX = 1;
-    coords.scaleY = 1;
-    coords.skewX = 0;
-    coords.skewY = 0;
-    coords.X = {0};
-    coords.Y = {0};
 
     std::random_device seedgen;
 
